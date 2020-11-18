09955f02b3d1188716bc3a187d672d0679d0ca2b
Trilisen <jonmcg01@gmail.com>
2020-11-18 15:05:45 +0100

    small fix to js

--- script.js
+++ script.js
@@ -4,6 +4,5 @@ function changeColor(color) {
         color.classList.remove("on");
     });
     const background = document.querySelector(`.background-${color}`);
-    console.log(background);
     background.classList.add("on");
 }

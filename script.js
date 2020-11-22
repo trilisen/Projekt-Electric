29b3452a56d3436fc9e8deaedbdd96d7cb652cad
rikardseg <70521759+rikardseg@users.noreply.github.com>
2020-11-22 13:44:26 +0100

    hamburgermenu + toggle

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ function changeColor(color) {
     const background = document.querySelector(`.background-${color}`);
     background.classList.add("on");
 }
+
+function hamburgerToggle() {
+    const menuContainer = document.querySelector(".menu-container");
+    menuContainer.classList.toggle("on");
+}

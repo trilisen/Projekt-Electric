886517c5e08e55d9806b4b39a956e27cde1ca248
Trilisen <jonmcg01@gmail.com>
2020-11-25 14:39:29 +0100

    Added function for changing background images

--- script.js
+++ script.js
@@ -11,3 +11,40 @@ function hamburgerToggle() {
     const menuContainer = document.querySelector(".menu-container");
     menuContainer.classList.toggle("on");
 }
+
+function changePage() {
+    const leftArrow = document.querySelector('.arrow.left');
+    const rightArrow = document.querySelector('.arrow.right');
+    let page = 0;
+    rightArrow.addEventListener('click', (e)=> {
+        const background = document.querySelector('.first-picture');
+        const leftArrow = document.querySelector('.arrow.left');
+        const rightArrow = document.querySelector('.arrow.right');
+        if (page === 0){
+            background.style.cssText = 'background: url(bilder/converted/norge.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            page++;
+            leftArrow.classList.add('on');
+        }else if (page === 1){
+            background.style.cssText = 'background: url(bilder/converted/tingstadstunneln.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            page++;
+            rightArrow.classList.remove('on');
+        }
+    });
+    leftArrow.addEventListener('click', (e)=> {
+        const background = document.querySelector('.first-picture');
+        const rightArrow = document.querySelector('.arrow.right');
+        if (page === 1){
+            background.style.cssText = 'background: url(bilder/converted/goteborg.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            page--;
+            e.target.classList.remove('on');
+        };
+        if (page === 2){
+            background.style.cssText = 'background: url(bilder/converted/norge.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            page--;
+            rightArrow.classList.add('on');
+        };
+    });
+    
+}
+
+changePage();

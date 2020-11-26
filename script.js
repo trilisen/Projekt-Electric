1a1e41cc62dde9790e2f48fb30226e1d3132d30e
Trilisen <jonmcg01@gmail.com>
2020-11-26 20:54:30 +0100

    Final commit before presentation

--- script.js
+++ script.js
@@ -22,15 +22,15 @@ function changePage() {
         const leftArrow = document.querySelector('.arrow.left');
         const rightArrow = document.querySelector('.arrow.right');
         if (page === 0){
-            background.style.cssText = 'background: url(img/cropped3.jpg) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            background.style.cssText = 'background: url(img/businessman.jpg) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
             background.role ="img";
             background.setAttribute('aria-label', "A white futuristic motorcycle with cyan neon details. A happy man in a business suit is standing to the right of it");
             page++;
             leftArrow.classList.add('on');
         }else if (page === 1){
-            background.style.cssText = 'background: url(img/cropped2.png) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            background.style.cssText = 'background: url(img/speed.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
             background.role="img";
-            background.setAttribute('aria-label', "Futuristic, pink motorcycle in a traffic tunnel.");
+            background.setAttribute('aria-label', "A white futuristic motorcycle with cyan neon details going fast in nature.");
             page++;
             rightArrow.classList.remove('on');
         }
@@ -39,14 +39,14 @@ function changePage() {
         const background = document.querySelector('.first-picture');
         const rightArrow = document.querySelector('.arrow.right');
         if (page === 1){
-            background.style.cssText = 'background: url(img/cropped1.png) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            background.style.cssText = 'background: url(img/gothenburg.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
             background.role="img";
             background.setAttribute('aria-label', "A futuristic, white motorcycle with neon details in a sunny, modern, inner city environment.");
             page--;
             e.target.classList.remove('on');
         };
         if (page === 2){
-            background.style.cssText = 'background: url(img/cropped3.jpg) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            background.style.cssText = 'background: url(img/businessman.jpg) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
             background.role="img";
             background.setAttribute('aria-label', "A white futuristic motorcycle with cyan neon details. A happy man in a business suit is standing to the right of it");
             page--;

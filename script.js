dd89c6d883ae11c6293b5d3eed50690d3b1f6e68
rikardseg <70521759+rikardseg@users.noreply.github.com>
2020-11-26 15:32:54 +0100

    merchbilder, cropped imgs

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ function changePage() {
         const leftArrow = document.querySelector('.arrow.left');
         const rightArrow = document.querySelector('.arrow.right');
         if (page === 0){
-            background.style.cssText = 'background: url(bilder/converted/norge.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            background.style.cssText = 'background: url(img/cropped3.jpg) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
             page++;
             leftArrow.classList.add('on');
         }else if (page === 1){
-            background.style.cssText = 'background: url(bilder/converted/tingstadstunneln.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            background.style.cssText = 'background: url(img/cropped2.png) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
             page++;
             rightArrow.classList.remove('on');
         }
@@ -35,12 +35,12 @@ function changePage() {
         const background = document.querySelector('.first-picture');
         const rightArrow = document.querySelector('.arrow.right');
         if (page === 1){
-            background.style.cssText = 'background: url(bilder/converted/goteborg.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            background.style.cssText = 'background: url(img/cropped1.png) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
             page--;
             e.target.classList.remove('on');
         };
         if (page === 2){
-            background.style.cssText = 'background: url(bilder/converted/norge.webp) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
+            background.style.cssText = 'background: url(img/cropped3.jpg) white; background-position: 50% 50%; background-repeat: no-repeat; background-size: cover';
             page--;
             rightArrow.classList.add('on');
         };
